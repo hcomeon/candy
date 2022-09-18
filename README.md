f86e114ae1d747826c7234fd6d1410335b10a148
Faithsky <11381828+Faithsky@user.noreply.gitee.com>
2022-09-18 17:31:14 +0800

    完成

---
+++ README.md
@@ -0,0 +1,12 @@
+# 糖果记账
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import "~@/assets/style/helper.scss";
+@import "~@/assets/style/reset.scss";
+body {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+  font-family: $font-hei;
+  font-size: 16px;
+  line-height: 1.5;
+  background: #fafafa;
+}
+
+#app {
+  max-width: 500px;
+  margin: 0 auto;
+}
+</style>
